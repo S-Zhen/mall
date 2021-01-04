8769fdfba87ef55aa73747824678e9b7cb099007
sunzhen <1025951770@qq.com>
2021-01-04 13:54:54 +0800

    开启注册中心

--- mall-coupon/src/main/java/com/sunzhen/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/sunzhen/mall/coupon/MallCouponApplication.java
@@ -3,9 +3,11 @@ package com.sunzhen.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.sunzhen.mall.coupon.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
     public static void main(String[] args) {
