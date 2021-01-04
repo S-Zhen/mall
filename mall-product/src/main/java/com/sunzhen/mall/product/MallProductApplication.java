3572fa40485fec8364392b0df5df5d78d15bc614
sunzhen <1025951770@qq.com>
2021-01-04 14:29:35 +0800

    开启feign(含测试方法)

--- mall-product/src/main/java/com/sunzhen/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/sunzhen/mall/product/MallProductApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 1、整合MyBatis-Plus
@@ -20,10 +21,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *          2、配置MyBatis-Plus；
  *              1）、使用@MapperScan
  *              2）、告诉MyBatis-Plus，sql映射文件位置
+ *
+ * 2、想要远程调用别的服务
+ *      1）、引入open-feign
+ *      2）、编写一个接口，告诉SpringCloud这个接口需要调用远程服务
+ *          1、声明接口的每一个方法都是调用哪个远程服务的那个请求
+ *      3）、开启远程调用功能
+ *
  */
 @MapperScan("com.sunzhen.mall.product.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.sunzhen.mall.product.feign")
 public class MallProductApplication {
 
     public static void main(String[] args) {
